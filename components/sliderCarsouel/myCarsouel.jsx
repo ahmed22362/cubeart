5a9803c1db89599c3d0e6e978f29a8a1c3416859
ahmedmangood <127854305+ahmedmangood@users.noreply.github.com>
2023-08-28 23:59:58 +0300

    updated carsouel and the responsive

--- components/sliderCarsouel/myCarsouel.jsx
+++ components/sliderCarsouel/myCarsouel.jsx
@@ -1,24 +1,45 @@
 "use client"
 
 import CarsouleItemSlider from './carsouleItem';
+import styles from './sliderCarsouel.module.css'
 
 export default function CarsouelSlider() {
     return(
         <div id="carouselExampleAutoplaying" className="carousel slide my-5" data-bs-ride="carousel">
         <div className="carousel-inner">
-            <CarsouleItemSlider 
+            <CarsouleItemSlider
                 headerText={"Let’s Bring your idea to life"}
                 carsouelText={"If you want to customize your product or design,contact us for partnership, support and more."}
                 firstBtn={"Print your design"} 
-                secBtn={"Get in touch"} 
+                secBtn={"Get in touch"}
+                btnColor={styles.yellow}
                 img={"carsouel-1.png"}
-                alt={"image 1"} />
-            {/* <CarsouleItemSlider 
-                headerText={"test test"} 
-                firstBtn={"Get in touch"} 
-                secBtn={"Get in touch"} 
+                alt={"image 1"}
+                active={"active"} />
+            <CarsouleItemSlider 
+                headerText={"Do you have a custom idea ?"}
+                carsouelText={"If you want to customize your product or design,contact us for partnership, support and more."}
+                firstBtn={"Get in Touch"} 
+                secBtn={"Get in touch"}
+                btnColor={styles.purple}
                 img={"carsouel-2.png"}
-                alt={"image 1"} /> */}
+                alt={"image 2"} />
+            <CarsouleItemSlider 
+                headerText={"Do you want to discover our services ?"}
+                carsouelText={"If you want to customize your product or design,contact us for partnership, support and more."}
+                firstBtn={"Get in Touch"} 
+                secBtn={"Get in touch"}
+                btnColor={styles.yellow}
+                img={"carsouel-3.png"}
+                alt={"image 3"} />
+            <CarsouleItemSlider 
+                headerText={"Do you want to discover our services ?"}
+                carsouelText={"If you want to customize your product or design,contact us for partnership, support and more."}
+                firstBtn={"Get in Touch"} 
+                secBtn={"Get in touch"}
+                btnColor={styles.purple}
+                img={"carsouel-2.png"}
+                alt={"image 2"} />
         </div>
         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
             <span className="carousel-control-prev-icon" aria-hidden="true"></span>
