68a88bef0fde7276f70c9e29da9f6f608781ffa2
ahmedmangood <127854305+ahmedmangood@users.noreply.github.com>
2023-09-02 18:45:11 +0300

    updated style

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9e61e9f316ba36c7"; }
+/******/ 		__webpack_require__.h = function() { return "68448a8a7bc69437"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
